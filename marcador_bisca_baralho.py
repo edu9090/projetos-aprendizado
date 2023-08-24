a6cb1867fdd1ce4c38650424dd294667470ecff8
edu9090 <142926786+edu9090@users.noreply.github.com>
2023-08-24 10:53:36 -0300

    Update marcador_bisca_baralho.py

--- marcador_bisca_baralho.py
+++ marcador_bisca_baralho.py
@@ -1,99 +1,113 @@
-import os
-import time
-run = True
-fs = True
-cond = True
-p1 = 0
-p2 = 0
-p3 = 0
-njog = 2
+import math
+import os 
 def limpar():
-    if os.name == ("posix"):
+    if os.name == "posix":
         os.system ("clear")
     else:
         os.system ("cls")
+def somar () :
+    soma1 = float(input("qual o primeiro valor : "))
+    soma2 = float(input("qual o segundo valor : "))
+    soma = soma1 + soma2
+    print ("a soma é ",soma)
     
+def subtrair () :
+    sub1 = float(input("qual o primeiro valor : "))
+    sub2 = float(input("qual o segundo valor : "))
+    sub = sub1 - sub2
+    print ("a subtração é ",sub)
+    
+def multy ():
+    multy1 = float(input("qual o primero valor :"))
+    multy2 = float(input("qual o valor a ser multiplicado : "))
+    multy = multy1 * multy2
+    print("a multiplicação é",multy)
 
-while run == True:
-    if fs == True:
-        limpar()
-        if njog > 3 or njog < 2:
-            print("retorne os valores validos : ")
-        njog = int(input("selecione 2 para dois jogadores ou duplas\nou digite 3 para trio\n-------------> : "))
-        fs = False
-        if njog > 3 or njog < 2:
-            run == True
-            fs == True
-    while cond == True:
-        limpar()
-        np1 = int(input("qual o numero de pontos do jogador 1 : "))
-        p1 = p1 + np1
-        limpar()
-        np2 = int(input("qual o numero de pontos do jogador 2 : "))
-        p2 = p2 + np2
-        if njog == 3:
-            limpar()
-            np3 = int(input("qual o numero de pontos do player 3 : "))
-            p3 = p3+np3
-        cond == False
-        break
-            
-        
+def divisão ():
+    div1 = float (input("qual o primeiro valor :"))
+    div2 = float (input("qual o valor a ser divido :"))
+    div = div1 / div2
+    print("a divisão é ",div)
+
+def fatorial():
+    xp = int(input("qual o número a ser fatorado: "))
+    fatorial = 1  
+    while xp > 0:
+        fatorial = fatorial * xp
+        xp = xp - 1
+    print("o fatorial é ",fatorial)
+
+print("O fatorial é", fatorial)
+
+def potencia ():
+    base = int (input("qual o numero base :"))
+    expoencia = int (input("qual o expoente : "))
+    potencia = base ** expoencia
+    print("a potencia é ",potencia)
+
+def raiz():
+    nraiz = int(input("qual o nuimero que você deseja a raiz quadrada : "))
+    raiz = nraiz ** 1/2 
+    print ("a raiz é ",raiz)
+
+def porcentagem ():
+    n1 = float (input("qual a porcentagem que deseja :"))
+    n2 = float (input("de que numero : "))
+    porcentagem = (n2 / 100) * n1
+    print("a porcentagem é ",porcentagem)
+
+def logaritimo ():
     limpar()
-    esc = int(input("1 para continuar\n2 para resetar\n3 para sair\n4 para os creditos : "))
-    if esc == 1:
-        run = True
-        cond = True
-    elif esc == 2:
-        p1 = 0
-        p2 = 0
-        np1 = 0
-        np2 = 0
-        if njog == 3:
-            p3 = 0
-            np3 = 0
-        fs = True
-        cond = True
-        run = True
-    else:
+    base = float(input("Qual é a base do logaritmo: "))
+    valor = float(input("Qual é o valor do logaritmo: "))
+    resultado = math.log(valor, base)
+    print("o logaritimo na base ",base ," de" ,valor," é ",resultado,)
+
+
+def menu():
+    fs = True
+    limpar()
+    if fs == True:
+        
+        esc = int(input("digite :\n1 para subtrair\n2 para somar\n3 para multiplicar\n4 para dividir\n5 para fatorial\n6 para potencia\n7 para raiz\n8 para porcentagem\n9 para logaritimo\n------------------> : "))
+        if esc != 1 and esc != 2 and esc != 3 and esc != 4 and esc != 5 and esc != 6 and esc != 7 and esc != 8 and esc != 9:
+            print("retorne valores validos")
+        limpar()
+        if esc == 1:
+            subtrair()
+        if esc == 2:
+            somar()
         if esc == 3:
-            break
-        elif esc == 4:
-            print("nome do desenvolvedor: EDUARDO VERUS DE BRITTO\n espere 10 segundos")
-            time.sleep (10)
-            run == True
-        else:
-            print ("retorne valores validos")
-limpar()
-if njog == 2 :
-    if p1>p2 :
-        print("o jogador ganhador é o player 1 com ",p1,"pontos\nseguido do player 2 com",p2,"pontos")
-    elif p2>p1 :
-        print("o jogador ganhador é o player 2 com ",p2,"pontos\nseguido do player 1 com",p1,"pontos")
-    else:
-        print("ambos empataram com ",p1," pontos")
-if njog ==3 :
-    if p1 > p2 and p1 > p3:
-        if p2 > p3:
-            print ("o ganhador é o player 1 com ",p1,"pontos\nseguido pelo player 2 com ",p2," pontos\ne o player 3 com ",p3,"pontos")
-        elif p3 > p2:
-            print ("o ganhador é o player 1 com ",p1," pontos\nseguido do player 3 com ",p3,"pontos\ne o player 2 com",p2,"pontos")
-        else:
-            print("o ganhador é o player 1 com",p1,"pontos \nseguido de um empate enrtre player 2 e player 3 com",p2,"pontos cada um")
-    elif  p2 > p1 and p2 > p3:
-        if p1>p3:
-            print ("o ganhador é o player 2 com ",p2,"pontos\nseguido pelo player 1 com ",p1," pontos\ne o player 3 com ",p3,"pontos")
-        elif p3>p1:
-            print ("o ganhador é o player 2 com ",p2,"pontos\nseguido pelo player 2 com ",p3," pontos\ne o player 3 com ",p1,"pontos")
-        else:
-            print("o ganhador é o player 2 com",p2,"pontos znseguido de um empate enrtre player 1 e player 3 com",p1,"pontos cada um")
-    else:
-        if p3 >p2 and p3 > p1:
-            if p1 > p2:
-                print ("o ganhador é o player 3 com ",p3,"pontos\nseguido pelo player 1 com ",p1," pontos\ne o player 2 com ",p2,"pontos")
-            elif p2 > p1:
-                print ("o ganhador é o player 3 com ",p3,"pontos\nseguido pelo player 2 com ",p2," pontos\ne o player 1 com ",p1,"pontos")
-            else:
-                print("o ganhador é o player 3 com",p3,"pontos znseguido de um empate enrtre player 1 e player 2 com",p2,"pontos cada um")
-        elif p1 == p2 and p1 == p3 and p2 == p3:
-            print("não sei como voces empataram com ",p1," pontos cada um ")
+            multy()
+        if esc == 4:
+            divisão()
+        if esc == 5:
+            fatorial()
+        if esc == 6:
+            potencia()
+        if esc == 7:
+            raiz()
+        if esc == 8:
+            porcentagem()
+        if esc == 9:
+            logaritimo()
+        fs = False
+        if esc != 1 and esc != 2 and esc != 3 and esc != 4 and esc != 5 and esc != 6 and esc != 7 and esc != 8 and esc != 9:
+            menu()
+            fs = True
+    
+    esc2 = int(input("1 para continuar\n2 para parar\n -----> : "))
+    limpar()
+    if esc2 == 1:
+        fs == True
+        menu()
+    elif esc2 == 2 :
+        posmenu()
+    else :
+        print ("retorne um valor valido")
+
+def posmenu():
+    aux = (input("codigo feito por :EDUARDO VERUS DE BRITTO\ntecle enter para finalizar............."))
+    exit()
+
+menu()
