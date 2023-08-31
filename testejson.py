b2eaccfb704c66cdb44529ba789f527e933db833
edu9090 <142926786+edu9090@users.noreply.github.com>
2023-08-31 14:47:27 -0300

    Create testejson.py

---
+++ testejson.py
@@ -0,0 +1,4 @@
+dadosJson = "{ \"titulo\" : \"json x xml\" , \"resumo\": \"o duelo de dois modelo de reprensentação de informações\" , \"anos\" : 2012, \"genero\" : [\"aventura\" , \"ação\" , \"ficção\"]}")
+f = open ("teste.json" , "w")
+f.write (dadosJson)
+f.close()
